69e8398d1505375d3aa6b2f3c11e6d610fa39921
Sasmita Basantia <sasmita@sasmitas-mbp.attlocal.net>
2020-06-07 21:22:52 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "subrat-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://mcnpclln5k.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_hpNo5XGUP",
+      APP_CLIENT_ID: "3iqb0t9nh7lc5r05vlh5fobqrl",
+      IDENTITY_POOL_ID: "us-east-2:258fa54f-56ec-4f9f-81fa-b9f38a7e8e25"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>New Age Fitness</h1>
+        <p>A simple dance fitness organizer</p>
+      </div>
+    </div>
+  );
+}
